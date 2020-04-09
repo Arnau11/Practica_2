ea5e536a26a34972901f56f681fd7efcac9e0689
gracimon <08-SET-99>
2020-04-09 12:23:38 +0200

    HelpScreen1 con barra de progreso

--- app/src/main/java/cat/urv/deim/asm/p2/common/SplashScreen.java
+++ app/src/main/java/cat/urv/deim/asm/p2/common/SplashScreen.java
@@ -4,10 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
 
-public class splash_screen extends AppCompatActivity {
-
+public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,9 +13,9 @@ public class splash_screen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(splash_screen.this, help_screen_1.class);
+                Intent intent = new Intent(SplashScreen.this, HelpScreen1.class);
                 startActivity(intent);
             }
-        },2000);
+        },4000);
     }
 }
