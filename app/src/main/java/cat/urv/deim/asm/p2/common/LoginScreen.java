31508a53c3539091c708938df38236bab9cedbb8
Arnau Porcel <aporcel1999@gmail.com>
2020-04-10 17:46:26 +0200

    login screen

--- app/src/main/java/cat/urv/deim/asm/p2/common/LoginScreen.java
+++ app/src/main/java/cat/urv/deim/asm/p2/common/LoginScreen.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LoginScreen extends AppCompatActivity {
+    public final static String USERS_NAME = "";
+    public final static String USERS_PASSWORD = "";
+    public final static boolean isAnonymous = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
