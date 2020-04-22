c5b41351a4ce2ebd365657ee487f7e6ec664103a
Arnau Porcel <aporcel1999@gmail.com>
2020-04-23 01:57:03 +0200

    fletxes per anar enrrere arreglades. El problema era que la main activity necessita que li passin extres sino peta.

--- app/src/main/java/cat/urv/deim/asm/p2/common/ProfileActivity.java
+++ app/src/main/java/cat/urv/deim/asm/p2/common/ProfileActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.lifecycle.Observer;
 import androidx.navigation.NavController;
@@ -24,11 +27,23 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        Window window = ProfileActivity.this.getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(ProfileActivity.this,R.color.colorPrimaryDark));
+
         setContentView(R.layout.activity_profile);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("User profile");
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+        intent.putExtra("isAnonymous", false);
+        startActivityForResult(intent, 0);
+        return true;
     }
 }
