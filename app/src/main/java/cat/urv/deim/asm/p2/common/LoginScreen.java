3e22637b1d432b82eb351546dd0701b5d641380f
Arnau Porcel <aporcel1999@gmail.com>
2020-04-22 20:42:45 +0200

    posar la toolbar a loginscreen un cop ja has entrat

--- app/src/main/java/cat/urv/deim/asm/p2/common/LoginScreen.java
+++ app/src/main/java/cat/urv/deim/asm/p2/common/LoginScreen.java
@@ -1,10 +1,12 @@
 package cat.urv.deim.asm.p2.common;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -18,20 +20,28 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
 
         setContentView(R.layout.activity_login_screen);
+
         Intent intent = this.getIntent();
 
         if (intent.getExtras() == null) {
             Button anonymousButton = (Button) findViewById(R.id.anonymousButton);
             anonymousButton.setText(getString(R.string.login_button_A));
+            Toolbar toolbar = findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+            getSupportActionBar().hide();
         } else {
             Button anonymousButton = (Button) findViewById(R.id.anonymousButton);
             anonymousButton.setText(getString(R.string.login_button_C));
-
+            Toolbar toolbar = findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setTitle("Login");
         }
 
 
@@ -71,6 +81,12 @@ public class LoginScreen extends AppCompatActivity {
         if(email.equals("sandra.adams@email.com") && pass.equals("123456")) {
             return true;
         } else return false;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivityForResult(myIntent, 0);
+        return true;
     }
 
 
