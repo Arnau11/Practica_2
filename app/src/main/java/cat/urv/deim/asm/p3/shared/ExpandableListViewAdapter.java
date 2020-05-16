822a8eb821d98180ef50ef7523a76949ec18ddcd
gracimon <08-SET-99>
2020-05-16 13:17:35 +0200

    Expandable class v1

--- app/src/main/java/cat/urv/deim/asm/p3/shared/ExpandableListViewAdapter.java
+++ app/src/main/java/cat/urv/deim/asm/p3/shared/ExpandableListViewAdapter.java
@@ -1,9 +1,11 @@
 package cat.urv.deim.asm.p3.shared;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
 
 import java.util.HashMap;
 import java.util.List;
@@ -34,22 +36,24 @@ public class ExpandableListViewAdapter extends BaseExpandableListAdapter {
 
     @Override
     public Object getGroup(int groupPosition) {
-        return null;
+        return this.questionList.get(groupPosition);
     }
 
     @Override
     public Object getChild(int groupPosition, int childPosition) {
-        return null;
+        return this.answerList.get(this.questionList.get(groupPosition)).get(childPosition);
     }
 
     @Override
     public long getGroupId(int groupPosition) {
-        return 0;
+        return groupPosition;
     }
+
+
 
     @Override
     public long getChildId(int groupPosition, int childPosition) {
-        return 0;
+        return childPosition;
     }
 
     @Override
@@ -59,16 +63,38 @@ public class ExpandableListViewAdapter extends BaseExpandableListAdapter {
 
     @Override
     public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-        return null;
+        String questionTitle = (String) getGroup(groupPosition);
+
+        if(convertView==null){
+            LayoutInflater inflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView=inflater.inflate(R.layout.question_tv,null);
+        }
+
+        TextView  questionTv = convertView.findViewById(R.id.question_tv);
+        questionTv.setText(questionTitle);
+
+        return convertView;
     }
+
+    //chapter=question y topic=asnwer
 
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        return null;
+        String asnwerTitle = (String) getChild(groupPosition, childPosition);
+
+        if(convertView==null){
+            LayoutInflater inflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView=inflater.inflate(R.layout.answer_list,null);
+        }
+
+        TextView  answerTv = convertView.findViewById(R.id.answer_tv);
+        answerTv.setText(asnwerTitle);
+
+        return convertView;
     }
 
     @Override
     public boolean isChildSelectable(int groupPosition, int childPosition) {
-        return false;
+        return true;
     }
 }
